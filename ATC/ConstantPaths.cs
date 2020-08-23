21aeb14e5988d0d0b9128ff16c517c9a46d9b877
Dominik Toczek <CRPW73@motorolasolutions.com>
2020-08-23 19:53:53 +0200

    test

--- ATC/ConstantPaths.cs
+++ ATC/ConstantPaths.cs
@@ -6,5 +6,6 @@ namespace ATC
     {
         public static readonly string rootDirPath = Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory()));
         public static readonly string configsPath = Path.Combine(rootDirPath, "Configs");
+        public static readonly string test = "asdasd";
     }
 }
